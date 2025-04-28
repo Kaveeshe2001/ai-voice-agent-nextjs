bf7675913f2e61649291e86b2a3d8602139c86bd
Kaveesha <kaveeshewaduge111@gmail.com>
2025-04-28 20:40:19 +0530

    Initial Setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2 className="text-yellow-200">Hello</h2>
+      <Button>Click here</Button>
+    </div>
+  );
+}
