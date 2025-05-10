43d1e46a797ec298637098c7f9b7da9e0abb02c4
Kaveesha <kaveeshewaduge111@gmail.com>
2025-05-10 16:26:46 +0530

    Interview Start page

--- app/interview/[interview_id]/start/page.tsx
+++ app/interview/[interview_id]/start/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+"use client"
+
+import { InterviewDataContext } from '@/context/InterviewDataContext'
+import { Mic, Phone, Timer } from 'lucide-react';
+import Image from 'next/image';
+import React, { useContext } from 'react'
 
 const StartInterview = () => {
+
+  const { interviewInfo, setInterviewInfo } = useContext(InterviewDataContext);
   return (
-    <div>
-      Start
+    <div className='p-20 lg:px-48 xl:px-56'>
+      <h2 className='font-bold text-xl flex justify-between'>
+        AI Interview Session 
+        <span className='flex items-center gap-2'>
+          <Timer />
+          00.00.00
+        </span>
+      </h2>
+
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-7 mt-5'>
+        <div className='bg-white h-[400px] flex flex-col gap-3 items-center justify-center rounded-lg border'>
+          <Image 
+            src={'/ai.jpg'}
+            alt='ai'
+            width={100}
+            height={100}
+            className='w-[60px] h-[60px] rounded-full object-cover'
+          />
+          <h2>AI Recruiter</h2>
+        </div>
+        <div className='bg-white h-[400px] flex flex-col gap-3 items-center justify-center rounded-lg border'>
+          <h2 className='text-2xl bg-primary text-white p-3 rounded-full px-5'>{interviewInfo?.userName[0]}</h2>
+          <h2>{interviewInfo?.userName}</h2>
+        </div>
+      </div>
+
+      <div className='flex items-center justify-center gap-5 mt-7'>
+        <Mic className='h-12 w-12 p-3 bg-gray-500 text-white rounded-full cursor-pointer'/>
+        <Phone className='h-12 w-12 p-3 bg-red-500 text-white rounded-full cursor-pointer'/>
+      </div>
+      <h2 className='mt-5 text-sm text-gray-400 text-center'>Interview in progress...</h2>
     </div>
   )
 }
